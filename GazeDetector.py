59357b38664799e569128f767260dab4f85a94ed
eliird <irdali1996@gmail.com>
2024-03-04 14:52:18 +0900

    Finished Gaze Detector

--- GazeDetector.py
+++ GazeDetector.py
@@ -1,12 +1,11 @@
-import math
-import time
-import cv2
+import math, time, cv2, torch
+import socket, struct, pickle
 import mediapipe as mp
 import numpy as np
 from PIL import Image
-import torch
 from torchvision import transforms
 from model import GazeLSTM
+
 
 class GazeDetector:
     def __init__(self, MAX_PEOPLE=5) -> None:
@@ -21,7 +20,7 @@ class GazeDetector:
 
         # define the gaze model
         model = GazeLSTM()
-        self.model = torch.nn.parallel(model)
+        self.model = torch.nn.DataParallel(model)
         checkpoint = torch.load('next_model/model_best_Gaze360.pth.tar', map_location=torch.device(self.device))
         self.model.load_state_dict(checkpoint['state_dict'])
         self.model.eval()
@@ -37,14 +36,40 @@ class GazeDetector:
         self.mp_drawing = mp.solutions.drawing_utils
         self.face_detection = mp.solutions.face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
     
-    def spherical2cartesial(x):    
+
+    def spherical2cartesial(self, x):    
         output = torch.zeros(x.size(0),3)
         output[:,2] = -torch.cos(x[:,1])*torch.cos(x[:,0])
         output[:,0] = torch.cos(x[:,1])*torch.sin(x[:,0])
         output[:,1] = torch.sin(x[:,1])
         return output
+    
 
-    def get_eye_bbox(detection, height, width):
+    def draw_gaze(self, pos,image_in, pitchyaw, thickness=2, color=(255, 255, 0),sclae=2.0):
+        
+        """
+        a= x_min
+        b = y_min
+        c =width
+        d= height
+        Draw gaze angle on given image with a given eye positions."""
+
+        image_out = image_in
+        (h, w) = image_in.shape[:2]
+        length = w/2
+        
+        if len(image_out.shape) == 2 or image_out.shape[2] == 1:
+            image_out = cv2.cvtColor(image_out, cv2.COLOR_GRAY2BGR)
+        dx = -length * np.sin(math.radians(pitchyaw[0])) * np.cos(math.radians(pitchyaw[1]))
+        dy = -length * np.sin(math.radians(pitchyaw[1]))
+
+        cv2.arrowedLine(image_out, (np.round(pos).astype(np.int32)),
+                    (np.round([pos[0] + dx, pos[1] + dy]).astype(int)), color,
+                    thickness, cv2.LINE_AA, tipLength=0.18)
+        return image_out    
+
+
+    def get_eye_bbox(self, detection, height, width):
         '''
         returns the position of eye gaze(x, y) and the bounding box around the detected face(x0, x1, y0, y1)
         '''
@@ -58,7 +83,7 @@ class GazeDetector:
         y0 = math.floor(max(0, ymin - heightF*0.5)*height)
         y1 = math.floor(min(height, y0+(heightF+ heightF)*height))
 
-        return ([(x0+x1)/2.0, (0.65*y0+0.35*y1)], [x0, x1, y0, y1])
+        return (((x0+x1)/2.0, (0.65*y0+0.35*y1)), [x0, x1, y0, y1])
 
 
     def process_image(self, image: np.array) -> np.array:
@@ -70,7 +95,7 @@ class GazeDetector:
         results = self.face_detection.process(image)
         if results.detections:
             
-            for i in range(len(results.detection), max_people):
+            for i in range(len(results.detections), max_people):
                 self.empty_frames[i] += 1
 
             for i, detection in enumerate(results.detections):
@@ -80,9 +105,9 @@ class GazeDetector:
 
                 self.empty_frames[i] = 0
                 # get the eye pos and bounding box from the detection
-                eyePos, boundBox = self.get_eye_pos(detection, h, w)
+                eyePos, boundBox = self.get_eye_bbox(detection, h, w)
                 # crop the faceImage and store it in the person_dict 
-                faceImage = self.transformation(image[boundBox[2]:boundBox[3], boundBox[0]:boundBox[1]])
+                faceImage = self.transformation(Image.fromarray(image[boundBox[2]:boundBox[3], boundBox[0]:boundBox[1]]))
                 self.person_dict[i].append((faceImage, eyePos))
         else:
             for i in range(max_people):
@@ -95,8 +120,9 @@ class GazeDetector:
         
         process_faces = []
         
-        for i, face_image_batch in enumerate(self.person_dict.values()):
+        for i, face_image_batch in enumerate(self.person_dict.values()):            
             if len(face_image_batch) == 7:
+                face_image_batch = [face[0] for face in face_image_batch]
                 process_faces.append(torch.stack(face_image_batch).reshape(21, 224, 224))
                 self.person_dict[i].pop(0)
         
@@ -104,12 +130,80 @@ class GazeDetector:
             return image
         
         process_faces = torch.stack(process_faces).to(self.device)
-        gazeVector, _ = self.model(process_faces)
-        gazeVector = gazeVector.detach().cpu() 
+        gazeVectors, _ = self.model(process_faces)
+        gazeVectors = gazeVectors.detach().cpu()
+        gazeVectors = self.spherical2cartesial(gazeVectors)
+        
+        for i, gaze_vector in enumerate(gazeVectors):
+            eyePos = self.person_dict[i][3][1]
+            tdx = int(eyePos[0])
+            tdy = int(eyePos[1])
+            self.draw_gaze((tdx, tdy), image, [math.degrees(gaze_vector[0]), math.degrees(gaze_vector[1])])
+        
+        print("_________________________________________________")
+        print(f"Detected {process_faces.shape[0]} Faces")
+        print(f"gaze_vectors: ", gazeVectors)
         return image
 
+    
+    def run_server(self):
 
-    def run(self):
+        # release the camera object
+        self.cap.release()
+  
+
+        server_ip = '127.0.0.1'  # Change this to your server IP
+        server_port = 9999
+
+        # Create socket object
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((server_ip, server_port))
+        server_socket.listen(5)
+
+        # Accept connection from client
+        client_socket, addr = server_socket.accept()
+        print("Connection from: ", addr)
+
+        # Start receiving video stream
+        data = b""
+        payload_size = struct.calcsize("L")
+
+        while True:
+            while len(data) < payload_size:
+                data += client_socket.recv(4096)
+
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0]
+
+            while len(data) < msg_size:
+                data += client_socket.recv(4096)
+
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            # Deserialize frame data
+            frame = pickle.loads(frame_data)
+
+            frame = self.process_image(frame)
+            
+            # Display received frame
+            cv2.imshow('Server', frame)
+            if cv2.waitKey(1) & 0xFF == 27:
+                self.cap.release()
+                cv2.destroyAllWindows()
+                break
+
+            # Send acknowledgment to client
+            client_socket.send(b"OK")
+
+        # Close sockets
+        client_socket.close()
+        server_socket.close()
+
+
+
+    def run_local(self):
         while True:
             start_time = time.time()
             
@@ -128,3 +222,5 @@ class GazeDetector:
                 break
             
             end_time = time.time()
+            fps = 1/(end_time - start_time)
+            print("FPS: ", fps)
--- run.py
+++ run.py
@@ -1,78 +1,11 @@
-import math
-import sys
-import time
-import torch
-import numpy as np
-import cv2
-
-# mediapipe imports
-import mediapipe as mp
-from mediapipe.tasks.python import vision
-from mediapipe.framework.formats import landmark_pb2
-
-# global declarations
-mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
-face_detection = mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.5)
-
-MAX_PEOPLE = 5
-
-
-def get_eye_pos(detection, height, width):
-    xmin = (detection.location_data.relative_bounding_box.xmin)
-    ymin = (detection.location_data.relative_bounding_box.ymin)
-    widthF =(detection.location_data.relative_bounding_box.width)
-    heightF = (detection.location_data.relative_bounding_box.height)
-    
-
-    x0 = math.floor(max(0, xmin - widthF*0.5)*width)
-    x1 = math.floor(min(width, x0 + (widthF+widthF)*width))
-    y0 = math.floor(max(0, ymin - heightF*0.5)*height)
-    y1 = math.floor(min(height, y0+(heightF+ heightF)*height))
-
-    eyePos = [(x0+x1)/2.0, (0.65*y0+0.35*y1)]
-    return eyePos
-
-def process_image(image: np.array) ->np.array:
-    '''
-    Take the image, compute the gaze and return the image with gaze drawn back
-    '''
-    h, w, c  = image.shape
-    results = face_detection.process(image)
-    if results.detections:
-        for i, detection in enumerate(results.detections):
-            eyePos = get_eye_pos(detection, h, w)
-
-
-    return image
+from GazeDetector import GazeDetector
 
 
 def main():
-    
-    cap = cv2.VideoCapture(0)
+    detector = GazeDetector()
+    detector.run_server()
+    # detector.run_local()
 
-    while True:
-        start_time = time.time()
-        
-        success, image = cap.read()
-       
-        if not success:
-            print("Image Not Read!")
-            continue
-
-        image = process_image(image)        
-        
-        cv2.imshow("X", image)
-        if cv2.waitKey(1) & 0xFF == 27:
-           cap.release()
-           cv2.destroyAllWindows()
-           break
-        
-        end_time = time.time()
-
-        
-    
 
 
 if __name__ == '__main__':
